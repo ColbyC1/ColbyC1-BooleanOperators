52b4945b0d64c06d0c9734abcc5e9de6d6dc21d8
Colby Canselo <colbycanselo@gmail.com>
2023-01-24 17:47:43 +0000

    Expressions

--- src/main/java/Expressions.java
+++ src/main/java/Expressions.java
@@ -13,8 +13,9 @@ public class Expressions {
      * @param b a true/false value.
      * @return if b is true, return true. if b is false, return false.
      */
+
     public boolean returnBoolean(boolean b){
-        return false;
+        return b == true;
     }
 
     /**
@@ -22,8 +23,9 @@ public class Expressions {
      * @return if b is true, return false. if b is false, return true.
      * look into boolean operations to do this without using if/else statements.
      */
+
     public boolean returnNot(boolean b){
-        return false;
+        return b != true;
     }
 
     /**
@@ -32,8 +34,9 @@ public class Expressions {
      * @return true if both b1 and b2 are true. false otherwise.
      * look into boolean operations to do this without using if/else statements.
      */
+
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return b1 && b2;
     }
 
     /**
@@ -42,8 +45,9 @@ public class Expressions {
      * @return true if either b1 or b1 are true. false if both are false.
      * look into boolean operations to do this without using if/else statements.
      */
+
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return b1 || b2;
     }
 
 
